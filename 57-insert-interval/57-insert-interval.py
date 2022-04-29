d835bfab5b06244327d7c036466f80625866e0f9
emkling <85776239+emkling@users.noreply.github.com>
2022-04-29 21:00:38 +0000

    Time: 124 ms (33.57%), Space: 17.4 MB (54.99%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -2,8 +2,6 @@ class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         result = []
         
-        
-        
         for i in range(len(intervals)):
             if intervals[i][0] > newInterval[1]:
                 result.append(newInterval)
@@ -15,7 +13,6 @@ class Solution:
             else:
                 newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
                 
-        
         result.append(newInterval)
         
         return result
