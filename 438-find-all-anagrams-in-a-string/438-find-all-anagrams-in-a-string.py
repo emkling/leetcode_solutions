d1765cb275676dd9266e86e89928421daf7d1827
emkling <85776239+emkling@users.noreply.github.com>
2022-05-03 15:58:34 +0000

    Time: 208 ms (48.51%), Space: 15.2 MB (34.68%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        window_start, matches = 0, 0
+        result = []
+        frequencies = {}
+        
+        for char in p:
+            if char not in frequencies:
+                frequencies[char] = 0
+            frequencies[char] += 1
+            
+         
+        for window_end in range(len(s)):
+            right_char = s[window_end]
+            
+            if right_char in frequencies:
+                frequencies[right_char] -= 1
+                
+                if frequencies[right_char] == 0:
+                    matches += 1
+                    
+            if matches == len(frequencies):
+                result.append(window_start)
+                
+                
+            if window_end - window_start + 1 >= len(p) :
+                left_char = s[window_start]
+                window_start += 1
+                
+                if left_char in frequencies:
+                    if frequencies[left_char] == 0:
+                        matches -= 1
+                        
+                    frequencies[left_char] += 1
+                    
+            
+        return result
